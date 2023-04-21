fa44dd6a7e6efaa155306c1a57facd0f8290f6bf
Nikka-tea-n <131405332+Nikka-tea-n@users.noreply.github.com>
2023-04-21 12:01:04 +0300

    Update MyPanel.java

--- src/MyPanel.java
+++ src/MyPanel.java
@@ -1,106 +1,98 @@
+
+
+import javax.swing.*;
 import java.awt.*;
-import java.util.LinkedList;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
-import static java.lang.Math.sqrt;
-public class Pendulum {
-    double m = 0.005; // масса в килограммах
+class MyPanel extends JPanel implements MouseListener, MouseMotionListener {
+    Point point;
+    public static long lastTime;
+    public static long currentTime;
 
-    double omega = 0;
-    double beta = 0;
-    double phi = 0;
-    double r; // в пикселях
-    double length; // в метрах ~ 100 пикселей
-    public LinkedList<Point> trace;
-
-    double attachmentPointX; // точка крепления (х)
-    double attachmentPointY; // точка крепления (у)
-
-    private double ballX; // положение груза (Х)
-    private double ballY; // положение груза (У)
-
-    //зависимый маятник - тот, который прикреплен к шару данного маятника
-    Pendulum dependentPendulum;
-
-    public Pendulum(double x0, double y0, double x, double y) {
-        this.attachmentPointX = x0;
-        this.attachmentPointY = y0;
-        this.ballX = x;
-        this.ballY = y;
-        recalculateLength();
-        recalculatePhi();
-        this.r = 13;
-        this.trace = new LinkedList<>();
-        omega = 0;
-        beta = 0;
+    public MyPanel(boolean isDoubleBuffered, Main main) {
+        super(isDoubleBuffered);
+        addMouseListener(this);
+        addMouseMotionListener(this);
     }
-
-    public void recalculateLength() {
-        this.length = sqrt(Math.pow(ballX - attachmentPointX, 2) + Math.pow(ballY - attachmentPointY, 2)) / 100;
-    }
-
-    public void recalculatePhi() {
-        phi = Math.atan2(ballX - attachmentPointX, ballY - attachmentPointY);
-    }
-
-    public double getBallX() {
-        return ballX;
-    }
-
-    public void setNewBallPosition(double ballX, double ballY) {
-        if (dependentPendulum != null) {
-            dependentPendulum.attachmentPointX = ballX;
-            dependentPendulum.attachmentPointY = ballY;
-            dependentPendulum.recalculateLength();
-            dependentPendulum.recalculatePhi();
+    //метод рисования с таймером, за внутри которого сразу вызывается перерисовка.
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        currentTime = System.nanoTime();
+        double deltaT = (currentTime - lastTime) / 1e9;
+        lastTime = currentTime;
+        if (Main.p1 == null || Main.p2 == null)
+            return;
+        g.setColor(new Color(23,28,35));
+        Main.p1.paintTrace(g);
+        Main.p2.paintTrace(g);
+        if(!Main.unvisible) {
+            g.setColor(Color.BLACK);
+            Main.p1.paintLine(g);
+            Main.p2.paintLine(g);
+            g.setColor(new Color(66, 195, 201));
+            Main.p1.paintBall(g);
+            g.setColor(new Color(119, 94, 158));
+            Main.p2.paintBall(g);
         }
-        this.ballX = ballX;
-        this.ballY = ballY;
-        recalculatePhi();
-        recalculateLength();
-    }
-    public void setBallX(double ballX) {
-        if (dependentPendulum != null)
-            dependentPendulum.attachmentPointX = ballX;
-        this.ballX = ballX;
+        if (Main.animationStarted) {
+            Main.updatePendulumSystem(deltaT);
+            MyPanel.this.repaint();
+        }
     }
 
-    public double getBallY() {
-        return ballY;
+    //обработчики
+    @Override
+    public void mousePressed(MouseEvent mouseEvent) {
+        point = mouseEvent.getPoint();
+        setDoublePendulum();
+        repaint();
     }
 
-    public void setBallY(double ballY) {
-        if (dependentPendulum != null)
-            dependentPendulum.attachmentPointY = ballY;
-        this.ballY = ballY;
+    @Override
+    public void mouseDragged(MouseEvent mouseEvent) {
+        point = mouseEvent.getPoint();
+        setDoublePendulum();
+        repaint();
     }
 
-    public Pendulum() {
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
     }
 
-    public void setDependentPendulum(Pendulum dependentPendulum) {
-        this.dependentPendulum = dependentPendulum;
-        this.dependentPendulum.attachmentPointX = this.ballX;
-        this.dependentPendulum.attachmentPointY = this.ballY;
-    }
-    public void paintTrace(Graphics g) {
-        // отрисовка следа
-        Point prevPoint = null;
-        int i = 0;
-        for (Point p: trace) {
-            i++;
-            if (prevPoint == null) {
-                prevPoint = p;
-                continue;
+
+//изменяет положение одного из шаров в зависимости от выбранного инструмента
+    private void setDoublePendulum() {
+        if (!Main.animationStarted) {
+            if (Main.curBall == 1) {
+                Main.p1.setNewBallPosition(point.x, point.y);
+            } else if (Main.curBall == 2) {
+                Main.p2.setNewBallPosition(point.x, point.y);
             }
-            g.drawLine(prevPoint.x, prevPoint.y, p.x, p.y);
-            prevPoint = p;
         }
-    }
-    // отрисовка маятника
-    public void paintBall(Graphics g) {
-        g.fillOval((int) (ballX - r), (int) (ballY - r), (int) (2 * r), (int) (2 * r));
-    }
-    public void paintLine(Graphics g) {
-        g.drawLine((int) attachmentPointX, (int) attachmentPointY, (int) ballX, (int) ballY);
     }
 }
+
+
+
+
+
